147625dabca05ade59283625dea70cc73178f361
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2022-08-26 10:48:17 -0400

    Move gone

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "null_resource" "animal" {
-  triggers = {
-    animal = var.animal
-  }
-}
-
 variable "animal" {
   type    = string
   default = "chicken"
@@ -12,4 +6,9 @@ variable "animal" {
 moved {
   from = null_resource.none
   to   = null_resource.animal
+}
+
+moved {
+  from = null_resource.animal
+  to   = null_resource.gone
 }
